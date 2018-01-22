22c9750e51e982adb66e611b3a6c7314b06eaa20
Glaucia Oliveira <gap.oliveira@yahoo.com.br>
2018-01-22 17:55:52 -0200

    Add project files.

---
+++ Xm.Atividade2/Xm.Atividade2/ViewModel/AlunoViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xm.Atividade2.Model;
+using Xm.Atividade2.View;
+
+namespace Xm.Atividade2.ViewModel
+{
+    public class AlunoViewModel : INotifyPropertyChanged
+    {
+        #region Propriedades
+        public Aluno AlunoModel { get; set; }
+        public ObservableCollection<Aluno> Alunos { get; set; } = new ObservableCollection<Aluno>();
+
+        // UI Events
+        public AdicionarAlunoCommand AdicionarAlunoCommand { get; }
+        public ICommand OnNovoCMD { get; private set; }
+        public ICommand OnSairCMD { get; private set; }
+        #endregion
+
+        public AlunoViewModel()
+        {
+            AlunoModel = new Aluno();
+            AdicionarAlunoCommand = new AdicionarAlunoCommand(this);
+            OnSairCMD = new Command(OnSair);
+            OnNovoCMD = new Command(OnNovo);
+        }
+
+        public void Adicionar(Aluno paramAluno)
+        {
+            try
+            {
+                if (paramAluno == null)
+                    throw new Exception("Usuário inválido");
+
+                paramAluno.Id = Guid.NewGuid();
+                Alunos.Add(paramAluno);
+                App.Current.MainPage.Navigation.PopAsync();
+            }
+            catch (Exception)
+            {
+                App.Current.MainPage.DisplayAlert("Falhou", "Desculpe, ocorreu um erro inesperado =(", "OK");
+            }
+        }
+
+        private async void OnSair()
+        {
+            await App.Current.MainPage.Navigation.PopAsync();
+        }
+
+        private async void OnNovo()
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new NovoAlunoView() { BindingContext = App.AlunoVM });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void EventPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    public class AdicionarAlunoCommand : ICommand
+    {
+        private AlunoViewModel alunoViewModel;
+        public AdicionarAlunoCommand(AlunoViewModel _alunoViewModel)
+        {
+            alunoViewModel = _alunoViewModel;
+        }
+        public event EventHandler CanExecuteChanged;
+        public void DeleteCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        public bool CanExecute(object parameter)
+        {
+            if (parameter != null) return true;
+
+            return false;
+        }
+        public void Execute(object parameter)
+        {
+            alunoViewModel.Adicionar(parameter as Aluno);
+        }
+    }
+}
